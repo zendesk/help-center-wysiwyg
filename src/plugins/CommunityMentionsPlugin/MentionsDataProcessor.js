c302125cf6c84a9e3a9f087391753cded81104f0
Gianluca Frediani <gianluca.frediani@zendesk.com>
2025-10-21 16:42:41 +0200

    Refactor MentionsDataProcessor to use ViewUpcastWriter for element manipulation
    With the new versions of CKEditor, the method used to convert the mention elements
    between the view and data formats has stopped working.
    
    This commit refactors the MentionsDataProcessor to use the proper CKEditor API for
    manipulating view elements, specifically the ViewUpcastWriter class.

--- src/plugins/CommunityMentionsPlugin/MentionsDataProcessor.js
+++ src/plugins/CommunityMentionsPlugin/MentionsDataProcessor.js
@@ -1,3 +1,5 @@
+import { ViewUpcastWriter } from "ckeditor5";
+
 // This custom DataProcessor is responsible for converting the rich text going to and from the server.
 // It is required due to the way the native CKEditor Mention plugin works.
 // The plugin expects the text content of the mention elements to always be the user's name,
@@ -11,16 +13,29 @@ class MentionsDataProcessor {
   // converts all <span data-mention> elements into <x-zendesk-user> elements.
   // The result is what will be sent to the server.
   toData(viewFragment) {
+    const writer = new ViewUpcastWriter(viewFragment.document);
+
     const findAndConvertMentionElements = (children) => {
       for (const child of children) {
         if (child.name === "span" && child.hasAttribute("data-mention")) {
           const userName = child.getAttribute("data-user-name");
-          child.name = "x-zendesk-user";
-          child.getChild(0)._textData = child.getAttribute("data-user-id");
+          const userId = child.getAttribute("data-user-id");
 
-          child._classes = new Set();
-          child._attrs = new Map();
-          child._attrs.set("data-user-name", userName);
+          const renamedElement = writer.rename("x-zendesk-user", child);
+
+          if (renamedElement.childCount > 0) {
+            const textNode = renamedElement.getChild(0);
+            if (textNode.is("$text")) {
+              writer.remove(textNode);
+              writer.appendChild(writer.createText(userId), renamedElement);
+            }
+          }
+
+          const attributeKeys = Array.from(renamedElement.getAttributeKeys());
+          for (const key of attributeKeys) {
+            writer.removeAttribute(key, renamedElement);
+          }
+          writer.setAttribute("data-user-name", userName, renamedElement);
         } else if (child.getChildren) {
           findAndConvertMentionElements(child.getChildren());
         }
@@ -44,7 +59,7 @@ class MentionsDataProcessor {
       const userName = oldMentionElement.getAttribute("data-user-name");
 
       const newMentionElement = parsedDOM.createElement("span");
-      newMentionElement.classList.add("mention");
+      newMentionElement.setAttribute("class", "mention");
       newMentionElement.setAttribute("data-user-id", userId);
       newMentionElement.setAttribute("data-mention", userName);
       newMentionElement.textContent = `@${userName}`;
